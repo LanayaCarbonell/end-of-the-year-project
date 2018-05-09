a8ffddf61c8d13d0b1d69ca184fed112415257d4
Lanaya Carbonell <31894552+LanayaCarbonell@users.noreply.github.com>
2018-05-09 16:35:33 -0400

    Create first week.py

---
+++ first week.py
@@ -0,0 +1,34 @@
+
+#NOTE TO ENRICO: this is my skeleton-I know I will add WAYYYY more and definitely drop somethings out the way!!!
+#This week I am only caring ab the FIRST STEP of my code! 5/9/18
+
+
+class AppCalorie(): #this is the full app 
+
+
+    def __import__(self): 
+        #This is where I will store my imported libraries and databases!! 
+        import math
+        import datetime
+        import time
+        #i need the food database library of calories!!
+    
+    def __init__(self): 
+        self.date = [] #in the span of twenty four hours!!
+        self.calories = [] #picking a FOOD to eat, giving it's calories!
+        self.daycalorie = []
+    
+    def __str__mealtime(self):
+        pass #this will give the reader a visiual on what they would like to choose/pick to eat: BREAKFAST/BRUNCH/LUNCH/DINNER/OR SNACK!! *will include time and date when inputting the BREAKFAST/BRUNCH/LUNCH/DINNER/OR SNACK
+    
+    def __str__choice(self):
+        pass #a string given to ask the user if they choose to end the amount of food they have eaten throughout the day to tally everything up!
+    
+    def __repr__endtime(self): 
+        pass #WILL RETURN THE CALORIE INPUT GIVEN BY THE END OF THE DAY!! 
+    #I also think this is good to asdd 
+    
+a = AppCalorie()
+print(a)
+
+        
