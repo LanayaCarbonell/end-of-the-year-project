a1e7bb130586ac4d357a8aa6bc4b5f60d8330df7
Lanaya Carbonell <31894552+LanayaCarbonell@users.noreply.github.com>
2018-05-13 22:06:11 -0400

    Create pt2_firstweek.py

---
+++ pt2_firstweek.py
@@ -0,0 +1,21 @@
+#PRACTICE!! 
+greeting = "Hello, welcome to calorie counter!" 
+a = 100
+b = 200
+c = 5
+class Meals: 
+    BREAKFAST = input('What would you like to Eat for Breakfast:')
+    
+    
+    BRUNCH = input('What what would you like to eat for Brunch:')
+    
+    
+    LUNCH = input('What would you like to eat for Lunch:')
+    
+    
+    SNACK = input('Did you have a snack today:')
+    
+    
+    DINNER = input('What did you eat a dinner:')
+    
+    print(BREAKFAST+BRUNCH+LUNCH+SNACK+DINNER)
