c3d470c61be7721c6545262e657b78b92215187c
Lanaya Carbonell <31894552+LanayaCarbonell@users.noreply.github.com>
2018-05-16 19:23:59 -0400

    Create week_two_pt1.py

---
+++ week_two_pt1.py
@@ -0,0 +1,28 @@
+import datetime
+class AppCounter: 
+    FOODS = [
+     'banana' == 105 , 'carrot' == 25, 'apple' == 45, 'grapefruit' == 52, 'coconut' == 1405, 'cherries' == 77, 'mango' == 201, 'avocado' == 234, 'kiwi' == 42, 'peach' == 59, 'lychee' == 6, 'plum' == 45, 'lime' == 20 ]
+     
+    def __init__(self): 
+        test.state = total
+        
+    def __repr__(self):
+       pass
+    
+    def food_intake(self, BREAKFAST, LUNCH, BRUNCH, DINNER, SNACK, total): 
+        BREAKFAST = input('What would you like to Eat for Breakfast:')
+        
+        LUNCH = input('What what would you like to eat for Lunch:')
+        
+        BRUNCH = input('What what would you like to eat for Brunch:')
+        
+        DINNER = input('What did you eat a dinner:')
+        
+        SNACK = input('Did you eat a snack:')
+        
+        total = BREAKFAST+LUNCH+BRUNCH+DINNER+SNACK
+        
+    def __str__(self): 
+        pass
+
+print(AppCounter)
