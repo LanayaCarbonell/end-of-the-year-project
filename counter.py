b104099ec7f6025998e3735efad75f4c09f552f9
Lanaya Carbonell <31894552+LanayaCarbonell@users.noreply.github.com>
2018-06-08 11:25:08 -0400

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,14 @@
+import sys
+import urllib
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+import datetime
+
+class AppCalorieCount(App):
+pass
